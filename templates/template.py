0512f62777896ee366c5fbdb3223afe5b64c79e8
Christian Dersch <lupinix@mailbox.org>
2017-05-05 17:40:29 +0200

    initial stuff

---
+++ templates/template.py
@@ -0,0 +1,6 @@
+#
+# Copyright (c) 2017 Christian Dersch <christian.dersch@physik.uni-marburg.de>
+#
+# License: MIT
+#
+
---
+++ templates/template_script.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2017 Christian Dersch <christian.dersch@physik.uni-marburg.de>
+#
+# License: MIT
+#
+
